3ced3f6f7907bc5af77cc68e6afd67ea6a098c46
kh4tib <34534018+kh4tib@users.noreply.github.com>
2018-05-07 16:24:35 +0300

    js filter using indexOf();

---
+++ script.js
@@ -0,0 +1,34 @@
+
+let searchForm = document.forms["form__search"];
+let searchList = document.forms["form__search"].querySelector("input");
+let list = document.querySelector(".form__list");
+
+// Closure so the function fires once only
+
+let notFound = (function(){
+    var executed = false;
+    return function() {
+        if (!executed) {
+            executed = true;
+            let notFoundMessage = document.createElement("li");
+            searchForm.appendChild(notFoundMessage);
+            notFoundMessage.textContent = "nothing found";
+            notFoundMessage.classList.add("not-found");
+        }
+    }
+})();
+
+// Filtering List
+searchList.addEventListener("keyup", function(ev){
+    let searchValue = ev.target.value.toLowerCase();
+    let items = document.querySelectorAll(".list__item");
+    Array.from(items).forEach(function(item){
+        let title = item.textContent;
+        if(title.toLowerCase().indexOf(searchValue) != -1){
+            item.style.display = "block";
+        } else {
+            item.style.display = "none";
+            notFound();
+        }
+    })
+}); 
